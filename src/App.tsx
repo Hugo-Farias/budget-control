ef5236d65a5026a6187572bef51d0935f9deb07e
Hugo-Farias <hg80rod@gmail.com>
2024-02-04 15:07:04 -0300

    sections added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.scss";
+import Group from "./components/Group";
 
 function App() {
-  return <h1>test</h1>;
+  return (
+    <div className={"app"}>
+      <div className="header">
+        <h1>header</h1>
+      </div>
+      <div className={"main"}>
+        <Group />
+      </div>
+      <div className="details">
+        <h1>details</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Group.tsx
@@ -0,0 +1,7 @@
+import "./Group.scss";
+
+const Group = function () {
+  return <h1>Main</h1>;
+};
+
+export default Group;
