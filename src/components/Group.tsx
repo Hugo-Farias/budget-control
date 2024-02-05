89cf5e4ed5399750f15a631dfeb4a3db78b70a1e
Hugo-Farias <hg80rod@gmail.com>
2024-02-05 14:36:39 -0300

    mobile first setup

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,7 +1,21 @@
 import "./Group.scss";
 
 const Group = function () {
-  return <h1>Main</h1>;
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+      commodi consectetur cumque dolore ducimus earum impedit, inventore ipsa
+      libero minus modi molestias natus neque odio odit officia pariatur
+      perspiciatis quo repellat sit unde veniam voluptatum. A ab ad adipisci
+      amet cum cumque, esse exercitationem facere fugiat incidunt iure labore
+      libero magni nulla porro quae quasi quibusdam tempora tempore voluptates!
+      Adipisci architecto beatae cupiditate dolores eum, facere inventore
+      molestias natus ratione voluptatibus! Accusamus accusantium ad atque
+      commodi consequatur consequuntur debitis deleniti dolorum et
+      exercitationem fugiat fugit hic incidunt natus non, officia omnis
+      perspiciatis porro quas quo rerum sequi temporibus unde voluptatibus.
+    </h1>
+  );
 };
 
 export default Group;
