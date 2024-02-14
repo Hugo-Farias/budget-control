181b107f3e69aa505b49ea316839653de99c278c
Hugo-Farias <hg80rod@gmail.com>
2024-02-14 16:10:33 -0300

    trying vite-plugin-style-import for global import

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import "./App.scss";
-import Group from "./components/Group";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className={"app"}>
-      <header className="header">
-        <h1>header</h1>
-      </header>
-      <div className="main">
-        <div className={"list"}>
-          <Group />
-        </div>
-        <div className="details">
-          <h1>details</h1>
-        </div>
-      </div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.scss";
+
+const Header = function () {
+  return (
+    <div className={"header"}>
+      <p className={"date"}>
+        Feb 2024 <div className={"triangle"} />
+      </p>
+    </div>
+  );
+};
+
+export default Header;
