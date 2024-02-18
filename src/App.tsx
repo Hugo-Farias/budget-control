6ad04b1a6e4e2bc2c91d3b73830d2cce6636b292
Hugo-Farias <hg80rod@gmail.com>
2024-02-18 19:05:10 -0300

    started toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header";
+import List from "./components/List";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <List />
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,13 @@
+import "./List.scss";
+
+const List = function () {
+  return (
+    <div className="list">
+      <div className="toolbar">
+        <div className="add-group">Add Group</div>
+      </div>
+    </div>
+  );
+};
+
+export default List;
