92baf5d7b4d082e3fa8252c58a34ebe8efc12b54
Hugo-Farias <hg80rod@gmail.com>
2024-02-19 19:05:05 -0300

    toolbar palceholder

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,6 +5,8 @@ const List = function () {
     <div className="list">
       <div className="toolbar">
         <div className="add-group">Add Group</div>
+        <div className="undo">Undo</div>
+        <div className="redo">Redo</div>
       </div>
     </div>
   );
