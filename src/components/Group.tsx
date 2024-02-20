3c4b853799989f8e0a761f5ee6cf99543aafc557
Hugo-Farias <hg80rod@gmail.com>
2024-02-20 15:58:03 -0300

    group UI in stisfactory state

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -2,7 +2,11 @@ import "./Group.scss";
 import { arrowDownSvg } from "../resources/svg";
 
 const Group = function () {
-  return <div className="group">{arrowDownSvg}Isaque</div>;
+  return (
+    <div className="group">
+      <div className="title">{arrowDownSvg}Isaque</div>
+    </div>
+  );
 };
 
 export default Group;
